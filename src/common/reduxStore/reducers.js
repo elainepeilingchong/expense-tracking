08532a6f205ce492442d6e25c2699a0b4c2facb2
Elaine Pei Ling Chong <elaine.chong@catapultsports.com>
2021-01-28 00:06:55 +0000

    add actions

--- src/common/reduxStore/reducers.js
+++ src/common/reduxStore/reducers.js
@@ -1,7 +1,9 @@
 
 import {combineReducers} from 'redux';
 import {reducer as formReducer} from 'redux-form';
+import reducer from './reducer';
 
 export default combineReducers({
     form: formReducer,
+    general: reducer
 });
